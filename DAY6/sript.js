2377d7bc8ef75ed129e6869b9c22ff7a340ba62c
hajeera banu <hajeerabanu95@gmail.com>
2024-07-11 19:08:37 +0530

    MAP RDUCE FILTER

---
+++ DAY6/sript.js
@@ -0,0 +1,111 @@
+
+// Basic for loop
+
+for (x = 1; x <= 10; x++){
+    console.log(x);
+}
+
+// using for loop with array
+
+var arr=[1,5,4,6,8,9]
+
+for (y = 0; y < arr.length; y++){
+    console.log(arr[y]);
+}
+
+// Using forEach for printing all the elements in the array
+
+arr.forEach( (value,index,self)=>{
+    console.log(value,index,self);
+})
+
+// MRF
+
+/* 
+ MAP-->Perform the specific action for each element in an array  and return the every element inside the new array
+ REDUCE
+ FILTER
+ */
+
+
+ 
+//   MAP
+
+var score = [4, 6, 8, 9, 7]
+var value = score.map((element) => { 
+    return element * 2;
+})
+console.log(value);
+
+
+// Filter
+
+// Using for each filter' array
+
+var input = ['🍒', '🍒', '🍒', "✿", '✿', '🟥']
+var output = [];
+
+// I want only [🍒,✿]
+// Forloop
+for (let x = 0; x < input.length; x++){
+    if (input[x] === '🍒' || input[x] === '✿') {
+        console.log(input[x]);
+        output.push(input[x])
+    }
+}
+
+console.log(output);
+
+// forEach
+
+var input = ['🍒', '🍒', '🍒', "✿", '✿', '🟥']
+var output = [];
+
+input.forEach((element ) => {
+    if (element !== '🟥') {
+       output.push(element)
+   }
+})
+console.log(input,output);
+
+// Using filter
+
+var output = input.filter((element) => {
+    return element !== '🟥' || element === '✿';
+})
+console.log(output);
+
+
+
+
+// Reduce
+
+// calculate for loop   using total score 
+var score = [4, 6, 8, 9, 7]
+var total = 0;
+for (let i = 0; i < score.length; i++){
+    total += score[i]
+}
+
+ console.log(total);
+var score = [4, 6, 8, 9, 7]
+var total = 0;
+
+score.forEach((element) => {
+     total += element;
+})
+console.log(total);
+
+// Reduce
+
+var score = [4, 6, 8, 9, 7]
+
+
+var val = score.reduce((carry, call)=>{
+
+   console.log(carry,call); 
+    return carry + call;
+
+})
+
+console.log(val);
--- Day5/OOPS.js
+++ Day5/OOPS.js
@@ -18,16 +18,76 @@ class food{
     getFoodtype() {
         return this.foodtype
     }
+ 
+    getQuantity() {
 
+        return this.quantity;
 
+   }
 
 }
-const idlie = new food('idlie', 30, ['rice', 'salt', 'fenugreek', 'gramdall'], 'Veg',
-    'https://www.thestatesman.com/wp-content/uploads/2019/04/idli.jpg',3);
+// const idlie = new food('idlie', 30, ['rice', 'salt', 'fenugreek', 'gramdall'], 'Veg',
+//     'https://www.thestatesman.com/wp-content/uploads/2019/04/idli.jpg',3);
 
-const dosa = new food('dosa', 50, ['rice', 'salt', 'fenugreek', 'gramdall', 'Oil'], 'Veg',
-    'https://www.awesomecuisine.com/wp-content/uploads/2009/06/Plain-Dosa.jpg',6);
+// const dosa = new food('dosa', 50, ['rice', 'salt', 'fenugreek', 'gramdall', 'Oil'], 'Veg',
+//     'https://www.awesomecuisine.com/wp-content/uploads/2009/06/Plain-Dosa.jpg',6);
 
 
-console.log(idlie.getItemPrice(), dosa.getItemPrice())
-console.log(idlie.getFoodtype(),dosa.getFoodtype());
+// class car {
+//     constructor(carname, module, colour, speed) {
+//         this.carname = carname;
+//         this.module = module;
+//         this.colour = colour;
+//         this.speed = speed;
+
+//         console.log(carname, module, colour, speed);
+            
+//     }
+
+
+//     getCarColour() {
+//         return this.colour;
+
+//     }
+
+
+//     getKilometer() {
+//         return this.speed;
+
+//     }
+// }
+    
+
+
+// const nixan = new car('nixan', "rc500", 'red', " 200 KM/perhour");
+// const maruti= new car('maruti','RC800','violet','800/kmperhour')
+
+
+ 
+// console.log(idlie.getItemPrice(), dosa.getItemPrice())
+// console.log(idlie.getFoodtype(), dosa.getFoodtype());
+// console.log(idlie.getQuantity(), dosa.getQuantity());
+// console.log(maruti.getCarColour(), nixan.getCarColour());
+
+/*
+* INHERITANCE
+*Derived class
+
+
+*/
+
+class SuperFood extends food  {
+
+    constructor( name, price, incrediant, foodtype, img, quantity, specialIncrediant = []) {
+        super(name, price, incrediant, foodtype, img, quantity);
+        this.specialIncrediant = specialIncrediant;  
+        
+
+    }
+}
+
+
+const AmbaniBreakfast = new SuperFood('idlie', 30, ['rice', 'salt', 'fenugreek', 'gramdall'], 'Veg',
+     'https://www.thestatesman.com/wp-conten t/uploads/2019/04/idli.jpg',3,['x','y']);
+
+console.log(AmbaniBreakfast);
